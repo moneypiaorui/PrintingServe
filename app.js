54eb4e11894888a95d9ae5d9488e2be209055f1c
幻离 <2451879312@qq.com>
2024-12-22 02:17:37 +0800

    增加gitignore

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(cors());
 
 
 // 设置静态文件目录
-app.use(express.static('dist'));
+app.use(express.static('public'));
 
 // 使用 body-parser 中间件解析请求体
 app.use(bodyParser.urlencoded({ extended: false }));
