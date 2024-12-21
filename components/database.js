64cdf28fd2697e7f17abef92492e9224e024b094
幻离 <2451879312@qq.com>
2024-12-22 02:15:00 +0800

    添加gitignore；database.js初始化添加admin管理员用户，userRole为admin

--- components/database.js
+++ components/database.js
@@ -10,11 +10,16 @@ const db = new sqlite3.Database('user.db', (err) => {
 });
 
 //启用外键约束
-    db.run("PRAGMA foreign_keys = ON");
+db.run("PRAGMA foreign_keys = ON");
 
 // 创建文件表
 db.serialize(() => {
     db.run("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT UNIQUE, password TEXT, userRole TEXT)");
+    db.get(`SELECT * FROM users WHERE username = "admin"`, function (err, row) {
+        if (!row) {
+            db.run(`INSERT INTO users (username, password,userRole) VALUES ("admin", "admin", "admin")`);
+        }
+    })
     db.run("CREATE TABLE IF NOT EXISTS files (id INTEGER PRIMARY KEY, username TEXT, filename TEXT,timestamp INTEGER, FOREIGN KEY(username) REFERENCES users(username))");
     db.run("CREATE TABLE IF NOT EXISTS printLogs (id INTEGER PRIMARY KEY, username TEXT, filename TEXT,printTimestamp INTEGER, pages INTEGER)");
 });
